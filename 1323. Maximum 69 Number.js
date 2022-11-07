14d069aa501ba649c495e116cc10d776abe7a89c
vinay kulkarni <94120295+vinaykulk621@users.noreply.github.com>
2022-11-07 23:06:36 +0530

    Create 1323. Maximum 69 Number.js

---
+++ 1323. Maximum 69 Number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    if(num.length==1){ 
+        return num 
+    }
+
+    num=num.toString().split("")
+    for(let i=0;i<num.length;i++){
+        if(num[i]==="6"){
+            num[i]="9"
+            break
+        }
+    }
+    num=num.join("")
+    return parseInt(num,10)
+};

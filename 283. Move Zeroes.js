fec845dc4ef5b73bd3a3e7de8f909a6841edbc7f
vinay kulkarni <94120295+vinaykulk621@users.noreply.github.com>
2022-11-11 09:21:48 +0530

    Update 283. Move Zeroes.js

--- 283. Move Zeroes.js
+++ 283. Move Zeroes.js
@@ -1,1 +1,12 @@
-
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    for(i=nums.length;i>=0;--i){
+        if(nums[i]!=0){
+            continue
+        }
+        nums.push(nums.splice(i,1))
+    }
+};

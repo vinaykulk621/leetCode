0801a31084a122e7df6471f61329339f191960d8
vinay <94120295+vinaykulk621@users.noreply.github.com>
2023-01-19 15:21:12 +0530

    Create 974. Subarray Sums Divisible by K.py

---
+++ 974. Subarray Sums Divisible by K.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        prefixMod,result=0,0
+        modGroups=[0]*k
+        modGroups[0]=1
+        for num in nums:
+            prefixMod=(prefixMod+num)%k
+            result+=modGroups[prefixMod]
+            modGroups[prefixMod]+=1
+        return result

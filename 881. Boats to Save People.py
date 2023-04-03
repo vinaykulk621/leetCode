06b12f07e23a2e156295732ebea8af4af07e19f7
vinay <94120295+vinaykulk621@users.noreply.github.com>
2023-04-03 22:11:36 +0530

    Create 881. Boats to Save People.py

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,12 @@
+# Use Double Pointer method
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left,right,boats=0,len(people)-1,0
+        while left<=right:
+            if people[left]+people[right]<=limit:
+                left+=1
+            right-=1
+            boats+=1
+        return boats

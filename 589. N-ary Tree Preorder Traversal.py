3bc3373fa2b202127a8ff01da7ff0f9cf0fd3084
vinay <94120295+vinaykulk621@users.noreply.github.com>
2023-04-21 16:34:53 +0530

    Create 589. N-ary Tree Preorder Traversal.py

---
+++ 589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        ans=[]
+        def trav(node):
+            if node is None:
+                return 
+            ans.append(node.val)
+            for child in node.children:
+                trav(child)
+        trav(root)
+        return ans
